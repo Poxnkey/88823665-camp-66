46c17b4cccfea833ee2e0130e4f97ddc73fe2a0c
zomdoi <naphat2706@gmail.com>
2024-12-18 17:42:55 +0700

    php1

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$my_arry = array(1,2,3,4,5);
+for ($i = 0; $i < count($my_arry); $i++) {
+}
+
+?>
+
+<h1><?php echo $my_arry[0]; ?></h1>
+
+<?php
+$myvar;
+$myvar = "Hello World!";
+echo "<h1>".$myvar."</h1><br>";
+$x = 1;
+{
+    $x = "ABS";
+}
+
+$my_array2[] = 2;
+print_r($my_arry);
+print_r($my_array2);
+
+if (TRue) {
+    
+} elseif (FALSE) {
+    
+}
+
+?>
