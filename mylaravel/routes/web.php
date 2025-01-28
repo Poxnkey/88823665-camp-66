28994378228dd06783f5e54fb502ccb66213f798
zomdoi <naphat2706@gmail.com>
2025-01-29 01:17:33 +0700

    adminlte

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', function () {
-    return view('home');
-});
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
 Route::get('/hello', function () {
     return "<h1>Hello World!</h1>";
@@ -13,3 +16,17 @@ Route::get('/hello', function () {
 
 Route::get('/mycontroller/{id?}', [MyController::class, 'myfunction']);
 Route::post('/mycontroller/{id?}', [MyController::class, 'myfunction']);
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::get('/404', function(){
+    abort(404);
+});
+
+
+Route::get('/500',function(){
+    abort(500);
+});
