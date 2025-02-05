2846a3bb285f7c5ec33fd140d9d17227758d718c
zomdoi <naphat2706@gmail.com>
2025-02-05 18:33:28 +0700

    edituser

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MyController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/home', function () {
@@ -20,13 +21,24 @@ Route::post('/mycontroller/{id?}', [MyController::class, 'myfunction']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'create']);
+
+Route::get('/users',
+[UserController::class, 'index']);
+Route::get('/user/{id}',
+[UserController::class, 'edit']);
+Route::put('/user',
+[UserController::class, 'edit_action']);
+Route::delete('/user',
+[UserController::class, 'delete']);
 
 Route::get('/404', function(){
     abort(404);
 });
 
 
-Route::get('/500',function(){
+Route::get('/500', function(){
     abort(500);
 });
